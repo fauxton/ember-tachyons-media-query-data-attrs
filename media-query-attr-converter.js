d5573436c7950982053e1eaac68ea2fd45f2c3b9
Curtis Ekstrom <code@fauxton.io>
2017-02-21 21:03:50 +0100

    Ensure elements w/o classes or data-attrs are untouched

--- media-query-attr-converter.js
+++ media-query-attr-converter.js
@@ -1,29 +1,67 @@
 /*jshint node:true*/
 
-function MediaQueryAttrConverter() {
+module.exports = class {
+  mockLocation() {
+    return {
+      source: null,
+      start: {},
+      stop: {}
+    };
+  }
+
+  mockClassAttr() {
+    return {
+      type: 'AttrNode',
+      name: 'class',
+      value: {
+        type: 'TextNode',
+        chars: '',
+        loc: this.mockLocation(),
+      },
+      loc: this.mockLocation(),
+    };
+  }
+
+  transform(ast) {
+    var walker = new this.syntax.Walker();
+
+    walker.visit(ast, (node) => {
+      if (node.type === 'ElementNode') {
+        let classAttr;
+
+        const presentClassAttr = node.attributes.find(attr => attr.name === 'class');
+
+        if (presentClassAttr) {
+          classAttr = presentClassAttr;
+        } else {
+          const mockClassAttr = this.mockClassAttr();
+          node.attributes.push(mockClassAttr);
+          classAttr = mockClassAttr;
+        }
+
+        console.log({classAttr});
+        let classes;
+        if (classAttr.value.chars) {
+          classes = classAttr.value.chars.split(' ');
+        } else {
+          classes = [];
+        }
+
+        node.attributes = node.attributes.filter((attr) => {
+          if(attr.name.startsWith('data-mq')) {
+            const [_prefix, suffix] = attr.name.split('data-mq');
+            const unsuffixedClasses = attr.value.chars.split(' ');
+            const suffixedClasses = unsuffixedClasses.map(klass => klass + suffix);
+            classes.push(...suffixedClasses);
+            return false;
+          }
+          return true;
+        });
+
+        classAttr.value.chars = classes.join(' ').trim();
+      }
+    });
+
+    return ast;
+  }
 }
-
-MediaQueryAttrConverter.prototype.transform = function(ast) {
-  var walker = new this.syntax.Walker();
-
-  walker.visit(ast, function(node) {
-    if (node.type === 'ElementNode') {
-      const classAttr = node.attributes.find(attr => attr.name === 'class');
-      let classes = classAttr.value.chars.split(' ');
-
-      const mqAttrs = node.attributes.filter(attr => attr.name.startsWith('data-mq'));
-      mqAttrs.map(attr => {
-        const [_prefix, suffix] = attr.name.split('data-mq');
-        const unsuffixedClasses = attr.value.chars.split(' ');
-        const suffixedClasses = unsuffixedClasses.map(klass => klass + suffix);
-        classes.push(...suffixedClasses);
-      })
-
-      classAttr.value.chars = classes.join(' ');
-    }
-  });
-
-  return ast;
-};
-
-module.exports = MediaQueryAttrConverter;
--- tests/integration/media-query-attr-convertor-test.js
+++ tests/integration/media-query-attr-convertor-test.js
@@ -6,7 +6,21 @@ moduleForComponent('class-generator-test', 'ConvertDataAttrsToClasses plugin', {
 });
 
 test('converts an arbitrary data-attr media query to the proper class name', function (assert) {
-  this.render(hbs`<div class="bg-black" data-mq-ns="w1 ph5">Sample Div</div>`);
+  this.render(hbs`
+    <article data-mq-l="ph1 pv3">
+      <div class="bg-black" data-mq-ns="w1 ph5">Sample Div</div>
+    </article>
+  `);
 
+  assert.equal(this.$('article').attr('class'), 'ph1-l pv3-l', 'Handles missing class attr');
   assert.equal(this.$('div').attr('class'), 'bg-black w1-ns ph5-ns', 'Appends class suffix');
+  assert.notOk(this.$('article').attr('data-mq-l'), 'removes data attributes');
+});
+
+test('ignores elements without data-attrs or classes', function (assert) {
+  this.render(hbs`
+    <article id="foobar">Foo Bar</article>
+  `);
+
+  assert.notOk(this.$('article').attr('class'), 'Does not add blank class attribute');
 });
